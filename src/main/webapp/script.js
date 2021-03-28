38015bfa868a937c03c03732f6d1835ff1628607
Carlo Lujan <carlouxljlj@gmail.com>
2021-03-28 23:00:38 +0000

    Initial commit

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -26,3 +26,17 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+var mymap = L.map('mapid').setView([-23.5505, -46.6333], 10);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoidmFsZW50aW5hc3BzIiwiYSI6ImNrbXFseXo5OTBpaXQycHQ0b2diYWZka2kifQ.Z5CYmHqc3fzrf8z-wxZYrg'
+}).addTo(mymap);
+
+var marker = L.marker([-23.5505, -46.633]).addTo(mymap);
+marker.bindPopup("<b>Hello world!</b><br>This is where Valentina is from!").openPopup();
