b1129196d4f373fbfedb3775d0eb528ea6b61018
Carlo Lujan <carlouxljlj@gmail.com>
2021-04-03 19:28:42 +0000

    Create location servlet, Filter location via incidents servlet, Create report Servlet, Put locations received as json inside the map, SQL Queries for: location insert, incident insert, location select and filtering, POST EXAMPLEgit add -A PLS READ @VALENTINA @KUI

---
+++ src/main/java/com/google/sps/servlets/CreateLocationReportServlet.java
@@ -0,0 +1,71 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@WebServlet("/Cl")
+public class CreateLocationReportServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
+	        throws ServletException, IOException {
+        //Data for the new Location object
+        Double longitude = Double.parseDouble(request.getParameter("longitude"));
+        Double latitude = Double.parseDouble(request.getParameter("latitude"));
+        String visualidentifier = request.getParameter("visualidentifier");
+        String gmapsid = request.getParameter("gmapsid");
+        int incidentmap = 1;
+        //Data for the new Incident object
+        int locationid = Integer.valueOf(request.getParameter("locationID"));
+        String typereports = request.getParameter("typeReports");
+        String note = request.getParameter("note");
+        //Ready to use date for sql query
+        java.util.Date utilDate = new java.util.Date();
+        java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());  
+        /*In this part we call the function that executes the query to 
+        add a new location and then a report*/
+        Boolean inserted = createNewLocationReport(request, longitude, latitude, visualidentifier, gmapsid, incidentmap, locationid, typereports, note, sqlDate);
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(inserted);
+        out.flush();   
+    }
+    public Boolean createNewLocationReport(HttpServletRequest request, double longitude,
+        double latitude,String visualidentifier, String gmapsid, int incidentmap, 
+            int locationID, String typereports, String note, java.sql.Date sqlDate) 
+                throws ServletException {
+        Boolean returnBoolean = false;
+        DataSource pool = (DataSource) request.getServletContext().getAttribute("my-pool");
+        try (Connection conn = pool.getConnection()) {
+            String stmt1 = "INSERT INTO locations values (NULL," +gmapsid+ ","+latitude+","+longitude+","+visualidentifier+",NULL,"+incidentmap+");";
+            String stmt2 = "INSERT INTO incidents values ("+locationID+ "," +typereports+ ","+note+","+sqlDate+",NULL);";
+            try (PreparedStatement locinsertStmt = conn.prepareStatement(stmt1)) {
+                ResultSet incidentResults = locinsertStmt.executeQuery();
+                System.out.println(incidentResults);
+            }
+            try (PreparedStatement repinsertStmt = conn.prepareStatement(stmt2)) {
+                ResultSet incidentResults = repinsertStmt.executeQuery();
+                System.out.println(incidentResults);
+            }
+            returnBoolean = true;
+        }catch (SQLException ex) {
+            returnBoolean = false;
+            throw new ServletException(
+                "Unable to successfully connect to the database.", ex);
+        }
+        return returnBoolean;
+    }
+}
---
+++ src/main/java/com/google/sps/servlets/LocationsServlet.java
@@ -0,0 +1,133 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import com.google.gson.Gson;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.google.sps.classes.Locationob;
+import java.sql.Blob;
+
+@WebServlet("/ls")
+public class LocationsServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
+	        throws ServletException, IOException, RuntimeException{
+
+        List<Locationob> locations = new LinkedList<Locationob>();
+        String incidents = request.getParameter("incidents");
+
+        if(!incidents.equals("none")){
+            switch(incidents){
+                case "all":
+                    incidents = ">=1";
+                break;
+                case "warning":
+                    incidents = ">1 AND incidentsmap <=5";
+                break;
+                case "danger":
+                    incidents = ">5 AND incidentsmap <= 10";
+                break;
+                case "dangerous":
+                    incidents = "> 10 AND incidentsmap <= 20";
+                break;
+                case "verydangerous":
+                    incidents = ">20";
+                break;
+                default:
+                    //In case someone changes the value in the html no error will appear, instead a list of all locations where incidents > 1
+                    incidents = ">=1";
+                break;
+            }
+            try {
+                //I don't know wich is correct I'll check once the html is ready
+                locations = rstolocobj(returnAllLocationsWhere(request, incidents)); 
+                //List<Locationob> locations_copy = new LinkedList<Locationob>(rstolocobj(returnAllLocationsWhere(request, incidents)));
+            }catch(Exception e){ 
+                System.out.println(e);
+            } 
+        }else{
+            //If both filters are None we return every location
+            try{
+                //I don't know wich is correct I'll check once the html is ready
+                locations = rstolocobj(returnAllLocations(request));
+                //List<Locationob> locations_copy = new LinkedList<Locationob>(rstolocobj(returnAllLocations(request)));
+            }catch(Exception e){ 
+                System.out.println(e);
+            } 
+        }
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(new Gson().toJson(locations));
+        out.flush();   
+    }
+
+    public ResultSet returnAllLocationsWhere(HttpServletRequest request, String incidents) throws SQLException, ServletException {
+        DataSource pool = (DataSource) request.getServletContext().getAttribute("my-pool");
+        try (Connection conn = pool.getConnection()) {
+            String stmt1 = "SELECT * FROM LOCATIONS WHERE incidentsmap"+incidents+";";
+            try (PreparedStatement getalllocationswhere = conn.prepareStatement(stmt1)) {
+                ResultSet locations = getalllocationswhere.executeQuery();
+                System.out.println(locations);
+                return locations;
+            }
+        }catch (SQLException ex) {
+            throw new ServletException(
+                "Unable to successfully connect to the database.", ex);
+        }
+    }
+
+    public ResultSet returnAllLocations(HttpServletRequest request) throws SQLException, ServletException {
+        DataSource pool = (DataSource) request.getServletContext().getAttribute("my-pool");
+        try (Connection conn = pool.getConnection()) {
+            String stmt1 = "SELECT * FROM LOCATIONS";
+            try (PreparedStatement getalllocations = conn.prepareStatement(stmt1)) {
+                ResultSet locations = getalllocations.executeQuery();
+                System.out.println(locations);
+                return locations;
+            }
+        }catch (SQLException ex) {
+            throw new ServletException(
+                "Unable to successfully connect to the database.", ex);
+        }
+    }
+
+    public List<Locationob> rstolocobj(ResultSet rs){
+        List<Locationob> locs = new LinkedList<Locationob>();
+        try {
+            while (rs.next()) {
+                int LocationID = rs.getInt(1);
+                String GmapsID = rs.getString(2);
+                Double Latitude = rs.getDouble(3);
+                Double Longitude = rs.getDouble(4);
+                String Visualidentifier = rs.getString(5);
+                Blob Image = rs.getBlob(6);
+                int Incidentsmap = rs.getInt(7);
+                Locationob loc = new Locationob(LocationID, GmapsID, Latitude, Longitude, Visualidentifier, Image,
+                        Incidentsmap);
+                locs.add(loc);
+                System.out.println("LocationID: " + LocationID + " GmapsID " + GmapsID + " Latitude: " + Latitude
+                        + " Longitude: " + Longitude + " Visualidentifier: " + Visualidentifier + " Image: " + Image
+                        + " " + Incidentsmap);
+            }
+        }catch(Exception e){ 
+                System.out.println(e);
+        } 
+        return locs;
+    }
+}
